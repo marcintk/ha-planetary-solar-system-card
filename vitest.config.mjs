3f69e7965963a0df7e5aa96ef4b1963f66398410
Marcin Kielichowski <mkielichowski@gmail.com>
2026-03-15 14:51:08 -0500

    Node actions are deprecated

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       include: ["src/**/*.js"],
-      reporter: ["text", "html", "json-summary"],
+      reporter: ["text", "html", "json-summary", "json"],
       reportsDirectory: "coverage",
     },
   },
